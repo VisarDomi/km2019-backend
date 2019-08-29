dc0d1505f99c4394c809e7390068a41f5b963c60
Visar Domi <visardomi4@gmail.com>
2019-08-30 01:53:28 +0200

    "any" put functionality

--- lambda_console-km2019-v2/artist/km2019-artist-post.js
+++ lambda_console-km2019-v2/artist/km2019-artist-post.js
@@ -1,33 +1,11 @@
 "use strict";
-
-const uuid = require("uuid/v4");
+const shared = require("shared-package");
 const AWS = require("aws-sdk");
+
 AWS.config.update({ region: "eu-west-1" });
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 exports.post = (event, context, callback) => {
-  let Item = JSON.parse(event.body);
-  const TableName = process.env.TABLENAME;
-  const id = uuid();
-  Item = { ...Item, id };
-  const params = { TableName, Item };
-
-  dynamoDb.put(params, error => {
-    if (error) {
-      console.error(error);
-      callback(null, {
-        statusCode: error.statusCode || 501,
-        headers: { "Content-Type": "text/plain" },
-        body: "Couldn't post the item."
-      });
-      return;
-    }
-
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(params.Item)
-    };
-    callback(null, response);
-  });
+  shared.postDynamoDb(event, callback, dynamoDb);
 };
