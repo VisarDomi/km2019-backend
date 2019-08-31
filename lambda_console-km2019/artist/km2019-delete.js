7ed6bf277aa850d9366b2f9e9bcbf3a1b121d62d
Visar Domi <visardomi4@gmail.com>
2019-08-31 12:23:07 +0200

    only 4 functions now put, delete, list, get

--- lambda_console-km2019/artist/km2019-delete.js
+++ lambda_console-km2019/artist/km2019-delete.js
@@ -1,5 +1,5 @@
 "use strict";
-const shared = require("shared-package");
+
 const AWS = require("aws-sdk");
 
 AWS.config.update({ region: "eu-west-1" });
@@ -7,5 +7,29 @@ AWS.config.update({ region: "eu-west-1" });
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 exports.delete = (event, context, callback) => {
-  shared.deleteDynamoDb(event, callback, dynamoDb);
+  const data = JSON.parse(event.body);
+  const TableName = data.TableName;
+  const Key = { id: data.id };
+  const params = {
+    TableName,
+    Key
+  };
+
+  dynamoDb.delete(params, (error, result) => {
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { "Content-Type": "text/plain" },
+        body: "Couldn't delete the item."
+      });
+      return;
+    }
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result)
+    };
+    callback(null, response);
+  });
 };
--- lambda_console-km2019/artist/km2019-put.js
+++ lambda_console-km2019/artist/km2019-put.js
@@ -1,11 +1,38 @@
 "use strict";
-const shared = require("shared-package");
+const uuid = require("uuid");
 const AWS = require("aws-sdk");
 
 AWS.config.update({ region: "eu-west-1" });
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-exports.put = async (event, context, callback) => {
-  await shared.postDynamoDb(event, callback, dynamoDb);
+exports.put = (event, context, callback) => {
+  let Item = JSON.parse(event.body);
+  let id = Item.id;
+  delete Item.id;
+  const TableName = Item.TableName;
+  delete Item.TableName;
+  if (id === undefined) {
+    id = uuid();
+  }
+  Item = { ...Item, id };
+  const params = { TableName, Item };
+
+  dynamoDb.put(params, error => {
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { "Content-Type": "text/plain" },
+        body: "Couldn't post the item."
+      });
+      return;
+    }
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item)
+    };
+    callback(null, response);
+  });
 };
